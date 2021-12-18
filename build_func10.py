4fd8aba87a8bf83e0747fefbad1080a3a645b3f4
Oybek <oybeksobitovxyz@gmail.com>
2021-12-18 12:44:29 +0500

    10

--- build_func10.py
+++ build_func10.py
@@ -1,2 +1,6 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
+x = 2
+y = 4
+answer = 3*y**(1/2)+x**(2/3)
+print(answer)
