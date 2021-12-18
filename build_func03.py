2371500b8aff66219486541c16b8e2630d4585da
Oybek <oybeksobitovxyz@gmail.com>
2021-12-18 11:04:36 +0500

    tets3

--- build_func03.py
+++ build_func03.py
@@ -1,2 +1,6 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func03
+def func3(n):
+    answer = (n+1)**2*3
+    return answer
+print(func3(3.5))
