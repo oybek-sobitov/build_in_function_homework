7736014e336fd4fd5935f013e458f1d103f2151c
Oybek <oybeksobitovxyz@gmail.com>
2021-12-18 12:47:39 +0500

    3

--- build_func03.py
+++ build_func03.py
@@ -1,5 +1,5 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func03
 n=3.5
-answer=(n+1)**2*3
+answer=pow((n+1),2)*3
 print(answer)
