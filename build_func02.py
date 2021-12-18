05592faad8b635cdb6d8e7b36d8b3b196b965fc2
Oybek <oybeksobitovxyz@gmail.com>
2021-12-18 11:01:02 +0500

    tets2

--- build_func02.py
+++ build_func02.py
@@ -1,2 +1,7 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
+def func2():
+    answer = 3*(7/5 + 9/4)
+    return answer
+
+print(func2())
